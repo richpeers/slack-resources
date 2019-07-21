9ca133089c3ead5db075fe7996980ee2cfdd8091
Rich Peers <richpeers1@gmail.com>
2019-07-22 00:12:59 +0100

    json_decode

--- app/Http/Resources/SlackResource.php
+++ app/Http/Resources/SlackResource.php
@@ -9,14 +9,14 @@ class SlackResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'meta' => $this->meta,
+            'meta' => json_decode($this->meta),
             'url' => $this->url
         ];
     }
